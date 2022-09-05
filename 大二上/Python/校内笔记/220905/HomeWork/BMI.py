bbd725e92a8acd1051a4a9f5e6eefa631854c004
mole9630 <1582616876@qq.com>
2022-09-05 18:14:15 +0800

    🔨修复了数据计算及判断的bug

--- 大二上/Python/校内笔记/220905/HomeWork/BMI.py
+++ 大二上/Python/校内笔记/220905/HomeWork/BMI.py
@@ -2,12 +2,12 @@ w = float(input('请输入体重(kg):'))
 h = float(input('请输入身高(m):'))
 BMI = w / (h * h)
 if BMI < 18.5:
-    print('身高为:', h, '体重为:', w, 'BMI指数为:', BMI, '您当前过轻')
-if BMI > 25:
-    print('身高为:', h, '体重为:', w, 'BMI指数为:', BMI, '您当前正常')
-if BMI > 28:
-    print('身高为:', h, '体重为:', w, 'BMI指数为:', BMI, '您当前过重')
+    print('身高为:', h, '体重为:', w, 'BMI指数为:', "%.3f" % BMI, '您当前过轻')
+if 25 > BMI >= 18.5:
+    print('身高为:', h, '体重为:', w, 'BMI指数为:', "%.3f" % BMI, '您当前正常')
+if 28 > BMI >= 25:
+    print('身高为:', h, '体重为:', w, 'BMI指数为:', "%.3f" % BMI, '您当前过重')
+if 32 > BMI >= 28:
+    print('身高为:', h, '体重为:', w, 'BMI指数为:', "%.3f" % BMI, '您当前肥胖')
 if BMI > 32:
-    print('身高为:', h, '体重为:', w, 'BMI指数为:', BMI, '您当前肥胖')
-else:
-    print('身高为:', h, '体重为:', w, 'BMI指数为:', BMI, '您当前严重肥胖')
+    print('身高为:', h, '体重为:', w, 'BMI指数为:', "%.3f" % BMI, '您当前严重肥胖')
