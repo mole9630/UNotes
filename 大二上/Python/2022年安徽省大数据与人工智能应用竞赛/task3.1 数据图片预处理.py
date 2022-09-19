7ccb65ada8388c7bebf83b47342eea810561028e
mole9630 <1582616876@qq.com>
2022-09-19 15:52:32 +0800

    🚀更新task3.1图片数据预处理代码笔记

---
+++ 大二上/Python/2022年安徽省大数据与人工智能应用竞赛/task3.1 数据图片预处理.py
@@ -0,0 +1,22 @@
+import cv2
+import os
+
+i = 0
+list_img = []
+resize_img = [1 for n in range(1, 6)]
+
+
+def read_directory(directory_name):
+    for filename in os.listdir(r"./" + directory_name):
+        img = cv2.imread(directory_name + "/" + filename)
+        list_img.append(img)
+
+
+read_directory('material/task3_1/')
+while i < 5:
+    resize_img[i] = cv2.resize(list_img[i], (128, 128))
+    resize_img[i] = cv2.blur(resize_img[i], (5, 5))
+    cv2.imshow('img_show', resize_img[i])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    i += 1
