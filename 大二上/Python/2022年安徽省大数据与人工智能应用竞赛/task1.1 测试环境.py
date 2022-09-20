8fe05ceaf0e726d8eaa10fc767e30954024c4c01
mole9630 <1582616876@qq.com>
2022-09-20 08:17:37 +0800

    🔨修复无法输出sklearn版本号问题

--- 大二上/Python/2022年安徽省大数据与人工智能应用竞赛/task1.1 测试环境.py
+++ 大二上/Python/2022年安徽省大数据与人工智能应用竞赛/task1.1 测试环境.py
@@ -5,7 +5,7 @@ import cv2
 print('Python version Info: ' + sys.version)
 print('PyTorch version Info: ' + torch.__version__ )
 print('OpenCV version Info: ' + cv2.__version__)
-# print("Sklearn verion Info { } ".format(sklearn.__version__))
+print("Sklearn verion is {}".format(sklearn.__version__))
 x1 = torch.zeros(2, 3)
 print("x1 :",x1)
 y1 = torch.ones(2, 3)
