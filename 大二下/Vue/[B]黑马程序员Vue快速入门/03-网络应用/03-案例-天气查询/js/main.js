ab1a76da70bdfcced6f2217c39992eb4dbb7d303
mole9630 <1582616876@qq.com>
2023-01-13 00:37:54 +0800

    ✨新增Vue网络应用全部知识笔记

---
+++ 大二下/Vue/[B]黑马程序员Vue快速入门/03-网络应用/03-案例-天气查询/js/main.js
@@ -0,0 +1,45 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		city: "",
+		weatherList: [],
+		msg: ""
+	},
+	methods: {
+		// 查询天气
+		searchWeather() {
+			// 保存this
+			const than = this;
+
+			// 清空weatherList
+			this.weatherList = [];
+
+			// 判断是否为空
+			if (this.city.trim().length != 0) {
+				// 调用接口
+				axios.get("https://v.api.aa1.cn/api/api-tianqi-3/index.php?msg=" + this.city + "&type=1").then((
+					res) => {
+					console.log(res);
+					console.log(res.data.data[0].wendu);
+					// 判断是否查询出数据
+					if (res.data.data[0].wendu != "～") {
+						// 保存天气数据
+						than.weatherList = res.data.data;
+						// console.log(than.weatherList);
+					} else {
+						alert("城市名错误, 请重试");
+					}
+				}, () => {
+					alert("网络繁忙, 请稍后再试");
+				})
+			} else {
+				alert("城市名不能为空");
+			}
+		},
+		// 单击文本框下的推荐天气
+		changeCity(city) {
+			this.city = city;
+			this.searchWeather();
+		}
+	}
+});
