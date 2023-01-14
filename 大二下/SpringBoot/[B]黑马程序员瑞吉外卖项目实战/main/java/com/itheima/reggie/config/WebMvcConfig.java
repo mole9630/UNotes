80b21dea8afd0a1bb7c962cc672f911120c7a0a2
mole9630 <1582616876@qq.com>
2023-01-14 19:50:59 +0800

    🔨修复雪花算法带来前端精度丢失的bug & 🚀新增自定义消息转换器

--- 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.itheima.reggie.config;
 
+import com.itheima.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -14,8 +19,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
      */
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("配置静态资源映射");
+//        log.info("配置静态资源映射");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 配置消息转换器
+     * @param converters 消息转换器
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(); // 创建消息转换器对象
+        // 设置对象转换器, 底层使用Jackson将Java对象转为JSON字符串
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将上面的消息转换器对象追加到mvc框架的转换器集合中
+        converters.add(0, messageConverter);
     }
 }
