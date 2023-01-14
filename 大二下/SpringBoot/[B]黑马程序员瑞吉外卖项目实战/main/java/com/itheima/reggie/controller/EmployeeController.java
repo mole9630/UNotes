4cc8c0baa4f407dd7628ce49fa866c4907703190
mole9630 <1582616876@qq.com>
2023-01-14 16:07:47 +0800

    ✨新增后台系统登录功能

--- 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -12,4 +20,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * @param request 请求
+     * @param employee 员工信息
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+        // 1.将页面提交的密码password进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        // 2.根据页面提交的用户名username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        // 3.如果没有查询到则返回登录失败结果
+        if (emp == null) {
+            return R.error("用户名错误, 请重新输入");
+        }
+
+        // 4.密码比对, 如果不一致则返回登录失败结果
+        if (!emp.getPassword().equals(password)) {
+            return R.error("密码错误, 请重新输入");
+        }
+
+        // 5.查看员工状态, 如果为已禁用状态, 则返回员工已禁用结果
+        if (emp.getStatus() == 0) {
+            return R.error("员工已被禁用, 请联系管理员");
+        }
+
+        // 6.登录成功, 将员Iid存入Session并返回登录成功结果
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
 }
--- 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/resources/backend/js/request.js
+++ 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/resources/backend/js/request.js
@@ -5,7 +5,7 @@
     // axios中请求配置有baseURL选项，表示请求URL公共部分
     baseURL: '/',
     // 超时
-    timeout: 10000
+    timeout: 1000000
   })
   // request拦截器
   service.interceptors.request.use(config => {
