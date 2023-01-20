c0033b9c15863466576b012002ddcc14f12e5675
mole9630 <1582616876@qq.com>
2023-01-20 13:20:18 +0800

    ✨新增菜品信息分页查询功能

--- 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/controller/DishController.java
+++ 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,14 +1,21 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.service.CategoryService;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品管理
@@ -20,6 +27,8 @@ public class DishController {
     private DishFlavorService dishFlavorService;
     @Autowired
     private DishService dishService;
+    @Autowired
+    private CategoryService categoryService; // 分类
 
     /**
      * 新增菜品
@@ -30,5 +39,41 @@ public class DishController {
     public R<String> save(@RequestBody DishDto dishDto) {
         dishService.saveWithFlavor(dishDto);
         return R.success("添加菜品成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        // 构造分页构造器
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>(page, pageSize);
+        // 构造条件构造器
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Dish::getName, name);
+        // 添加排序条件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+        // 查询
+        dishService.page(pageInfo, queryWrapper);
+        // 对象拷贝
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records"); // 不拷贝records属性
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> dishDtoList = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item, dishDto); // 拷贝属性
+
+            Long categoryId = item.getCategoryId(); // 获取菜品分类ID
+            // 根据分类ID查询分类
+            Category category = categoryService.getById(categoryId);
+
+            // 检查分类是否为空
+            if (category != null) {
+                String categoryName = category.getName(); // 获取分类名称
+                dishDto.setCategoryName(categoryName); // 设置分类名称
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+        dishDtoPage.setRecords(dishDtoList);
+
+        return R.success(dishDtoPage);
     }
 }
