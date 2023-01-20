0b0b4e3e97426dd78982f921bdf5732dd4e41512
mole9630 <1582616876@qq.com>
2023-01-20 23:36:47 +0800

    ✨新增用户端添加购物车功能

---
+++ 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/controller/ShoppingCartController.java
@@ -0,0 +1,54 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.BaseContext;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.ShoppingCart;
+import com.itheima.reggie.service.ShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/shoppingCart")
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        // 设置当前用户id, 指定当前哪个用户添加的购物车
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, currentId);
+
+        // 当前添加的菜品/套餐是否在购物车中
+        Long dishId = shoppingCart.getDishId();
+        if (dishId != null) {
+            // 添加到购物车的是菜品
+            queryWrapper.eq(ShoppingCart::getDishId, dishId);
+        } else {
+            // 添加到购物车的是套餐
+            queryWrapper.eq(ShoppingCart::getSetmealId, shoppingCart.getSetmealId());
+        }
+
+        //SQL:select * from shopping_cart where user_id = ? and dish_id/setmeal_id = ?
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(queryWrapper);
+        if (cartServiceOne != null) {
+            // 表示已经存在, 在原数量基础上+1
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            // 若不存在, 则添加到购物车中, 数量默认为1
+            shoppingCart.setNumber(1);
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+        return R.success(cartServiceOne);
+    }
+}
---
+++ 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,11 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.entity.ShoppingCart;
+import com.itheima.reggie.mapper.ShoppingCartMapper;
+import com.itheima.reggie.service.ShoppingCartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShoppingCartServiceImpl extends ServiceImpl<ShoppingCartMapper, ShoppingCart> implements ShoppingCartService {
+}
