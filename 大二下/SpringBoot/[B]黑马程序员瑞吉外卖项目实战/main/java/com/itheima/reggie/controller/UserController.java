c9f8fbb94f37bd7dabd802a717524ba9fdc9a388
mole9630 <1582616876@qq.com>
2023-01-27 13:04:44 +0800

    ✨新增redis依赖 & 🚀优化将短信验证码存入redis并设置5分钟有效

--- 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/controller/UserController.java
+++ 大二下/SpringBoot/[B]黑马程序员瑞吉外卖项目实战/main/java/com/itheima/reggie/controller/UserController.java
@@ -9,6 +9,7 @@ import com.itheima.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +25,8 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 发送手机验证码
@@ -37,14 +41,15 @@ public class UserController {
         if (StringUtils.isNotEmpty(phone)) {
             // 生成随机的6位数验证码
             String code = ValidateCodeUtils.generateValidateCode(6).toString();
-            log.info("手机号{}的验证码为:{}", phone, code);
+            log.info("手机号{}的验证码为:{} (5分钟内有效)", phone, code);
 
             // 调用短信服务发送验证码
 //            SMSUtils.sendMessage("签名", "模板编码", phone, code);
 
             // 将验证码存入session
-            session.setAttribute(phone, code);
-
+//            session.setAttribute(phone, code);
+            // 将验证码存入redis中并设置有效期为5分钟
+            redisTemplate.opsForValue().set(phone, code, 300, TimeUnit.SECONDS);
             return R.success("验证码发送成功");
         }
         return R.error("验证码发送失败");
@@ -63,9 +68,11 @@ public class UserController {
         // 获取验证码
         String code = map.get("code").toString();
         // 获取session中的验证码
-        Object codeInSession = session.getAttribute(phone);
-        // 进行验证码比对(提交的验证码 和 session保存的验证码比对)
-        if (codeInSession != null && codeInSession.equals(code)) {
+//        Object codeInSession = session.getAttribute(phone);
+        // 获取redis中的验证码
+        Object codeInRedis = redisTemplate.opsForValue().get(phone);
+        // 进行验证码比对(提交的验证码 和 redis保存的验证码比对)
+        if (codeInRedis != null && codeInRedis.equals(code)) {
             // 验证码正确,登录成功
             // 判断手机号是否为新用户, 若是自动完成注册
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
@@ -79,6 +86,10 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user", user.getId());
+
+            // 如果登录成功, 将验证码从redis中删除
+            redisTemplate.delete(phone);
+
             return R.success(user);
         }
         // 验证码错误, 登录失败
